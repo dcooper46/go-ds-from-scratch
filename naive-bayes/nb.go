c3656457aed58a6424283e8eac2b39c659a678d0
dcooper46 <djcoop46@yahoo.com>
2020-09-29 19:01:13 -0500

    add full classifier

--- naive-bayes/nb.go
+++ naive-bayes/nb.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math"
 	"regexp"
 	"strings"
 )
@@ -14,10 +15,20 @@ type record struct {
 	hit     bool
 }
 
-type wordprob struct {
+// Wordprob holds the hit and miss probabilities for a word
+type Wordprob struct {
 	word     string
 	hitprob  float64
 	missprob float64
+}
+
+func stringInList(s string, list []string) bool {
+	for _, sl := range list {
+		if s == sl {
+			return true
+		}
+	}
+	return false
 }
 
 // Tokenize breaks a text string into unique alphanumeric words
@@ -30,10 +41,10 @@ func Tokenize(message string) []string {
 
 	wordMap := make(map[string]struct{})
 	for _, w := range words {
-		wordMap[w] = EXISTS
+		wordMap[string(w)] = EXISTS
 	}
 
-	uniquewords := make([]string, "", len(wordMap))
+	uniquewords := make([]string, len(wordMap))
 	var i int
 	for word := range wordMap {
 		uniquewords[i] = word
@@ -65,14 +76,14 @@ func CountWords(records []record) map[string]map[string]int {
 
 // WordProbs calculates the probability of a `hit` and `miss`
 // for each work in the corpus.
-func WordProbs(counts map[string]ma[string]int, nhits, nmisses int, k float64) {
+func WordProbs(counts map[string]map[string]int, nhits, nmisses int, k float64) []Wordprob {
 	if k == 0.0 {
 		k = 0.5
 	}
-	wordprobs := make([]wordprob, len(counts))
+	wordprobs := make([]Wordprob, len(counts))
 	var i int
 	for word, counts := range counts {
-		wordprobs[i] = wordprob{
+		wordprobs[i] = Wordprob{
 			word:     word,
 			hitprob:  (float64(counts["hit"]) + k) / (float64(nhits) + 2*k),
 			missprob: (float64(counts["miss"]) + k) / (float64(nmisses) + 2*k),
@@ -80,4 +91,54 @@ func WordProbs(counts map[string]ma[string]int, nhits, nmisses int, k float64) {
 		i++
 	}
 	return wordprobs
+}
+
+// HitProb assigns a probability to a message using
+// its message and precomputed word probabilities
+func HitProb(wordprobs []Wordprob, message string) float64 {
+	words := Tokenize(message)
+	hitLogProb, missLogProb := 0.0, 0.0
+
+	for _, wp := range wordprobs {
+		if stringInList(wp.word, words) {
+			hitLogProb += math.Log(wp.hitprob)
+			missLogProb += math.Log(wp.missprob)
+		} else {
+			hitLogProb += math.Log(1.0 - wp.hitprob)
+			missLogProb += math.Log(1.0 - wp.missprob)
+		}
+	}
+
+	hitProb := math.Exp(hitLogProb)
+	missProb := math.Exp(missLogProb)
+
+	return hitProb / (hitProb + missProb)
+}
+
+// NaiveBayesClassifier implements a simple naive bayes algorithm
+// useful for a small binary classification problem
+type NaiveBayesClassifier struct {
+	k         float64
+	wordprobs []Wordprob
+}
+
+// Train calculates word probabilities given a
+// training set of messages
+func (nb *NaiveBayesClassifier) Train(data []record) {
+	var nhits, nmisses int
+	for _, rec := range data {
+		if rec.hit {
+			nhits++
+		}
+	}
+	nmisses = len(data) - nhits
+
+	wordcounts := CountWords(data)
+	nb.wordprobs = WordProbs(wordcounts, nhits, nmisses, nb.k)
+}
+
+// Classify predicts whether a message should be considered
+// a `hit` or a `miss`
+func (nb *NaiveBayesClassifier) Classify(message string) float64 {
+	return HitProb(nb.wordprobs, message)
 }
